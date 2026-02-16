a8dbe45fb6acad84da99973f565e1dd3d9cb3607
yaboyATOMIC <villabong7796@gmail.com>
2026-02-16 22:08:29 +0800

    Add files via upload

---
+++ ILOVEYOU.js
@@ -0,0 +1,5 @@
+const textElement = document.getElementById('text-portrait');
+const message = "I LOVE YOU ";
+// Repeat the message enough times to cover the portrait area (e.g., 5000 times)
+const repeatedMessage = message.repeat(5000); 
+textElement.textContent = repeatedMessage;
